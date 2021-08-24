e7d84cf970f7d5905501f4db1b83f7c5041a7054
Gino Swanepoel <ginoleemusic@live.com>
2021-08-25 00:04:08 +0200

    need to remove unused providers

--- App.js
+++ App.js
@@ -8,14 +8,18 @@ import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import Home from "./app/screens/Home";
 import Profile from "./app/screens/Profile";
 import NavBar from "./app/components/NavBar";
+import { ScrollProvider } from "./app/config/config";
+import { SliderProvider } from "./app/context/SliderContext";
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <BillProvider>
-      <NavigationContainer>
-        {/* <Stack.Navigator
+    <ScrollProvider>
+      <SliderProvider>
+        <BillProvider>
+          <NavigationContainer>
+            {/* <Stack.Navigator
         screenOptions={{
           headerShown: false,
         }}
@@ -23,8 +27,10 @@ export default function App() {
         <Stack.Screen name="Home" component={Home} />
         <Stack.Screen name="Profile" component={Profile} />
       </Stack.Navigator> */}
-        <NavBar />
-      </NavigationContainer>
-    </BillProvider>
+            <NavBar />
+          </NavigationContainer>
+        </BillProvider>
+      </SliderProvider>
+    </ScrollProvider>
   );
 }
